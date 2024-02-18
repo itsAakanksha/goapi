69c26d6674017dbe2b0974fcf26262d55c745f7a
Aakanksha <82.25aakanksha@gmail.com>
2024-02-18 14:18:12 +0530

    built api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,24 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"github.com/itsAakanksha/goapi/internal/handlers"
 	log "github.com/sirupsen/logrus"
 )
+
+func main(){
+
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("starting go app service")
+	fmt.Println(`
+	GO API
+	`)
+
+	err:=http.ListenAndServe("localhost:8000",r)
+	if err!=nil{
+		log.Error(err)
+	}
+}
